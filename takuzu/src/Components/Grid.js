3a184fb638d1daee14acdccb2857ebfef1da7f0c
Anthony Fabius <afabius05@gmail.com>
2023-08-23 11:40:32 -0400

    Adding tiles val matrix
    not stable yet btw

--- takuzu/src/Components/Grid.js
+++ takuzu/src/Components/Grid.js
@@ -5,34 +5,55 @@ import './Grid.css';
 // Component imports
 import Tile from './Tile';
 
+let id = 0; // Unique id so every tile rerenders
+
 function Grid({n, puzzle_}) {
   const[tiles, setTiles] = useState([]);
+  const[tilesVal, setTilesVal] = useState([]);
+
+  function onTileClick(val, row, col){
+    const temp = tilesVal.map((rowArr, i) =>
+      i === row ? rowArr.map((cell, j) =>
+      (j === col ? val : cell)) : rowArr
+    );
+    setTilesVal(temp);
+  }
   
   // Loads puzzles
   function loadPuzzle(){
     let newTiles = [];
+    let newTilesVal = [];
     for(let i=0; i<n; i++){
       newTiles.push([]);
+      newTilesVal.push([]);
       for(let j=0; j<n; j++){
+        newTilesVal[i].push(puzzle_[i][j]);
         newTiles[i].push(
           <Tile
-            value_={puzzle_[i][j]}
-            mutability={(puzzle_[i][j] === 2)}
-            n={n}
+            key = {`${i}-${j}-${id}`}
+            value_= {puzzle_[i][j]}
+            mutability = {(puzzle_[i][j] === 2)}
+            n = {n}
+            setVal = {onTileClick}
+            pos = {i*n+j}
           />);
+        id+=1;
       }
     }
     setTiles(newTiles);
+    setTilesVal(newTilesVal);
   }
 
   // Reloads board whenever puzzle_ is altered
   useEffect(() => {
-    if(puzzle_)
-      loadPuzzle();
-  });
+    loadPuzzle();
+    console.log(tilesVal);
+  },[puzzle_]);
 
   return(
-    <div className="base">{tiles}</div>
+    <div className="base">
+      {tiles.map((tile) => tile)}
+    </div>
   );
   
 }
--- takuzu/src/Components/Tile.js
+++ takuzu/src/Components/Tile.js
@@ -1,19 +1,26 @@
 // React imports
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import './Tile.css';
 
-function Tile({value_, mutability, n}){
+function Tile({value_, mutability, n, setVal, pos}){
   const [value, setValue] = useState(value_);
   let color;
   let style;
+
+  // Determines Tile Size
   if(n===6) style = {height: "75px", width: "75px", margin: "2px", fontSize: "40px"};
   else if(n===8) style = {height: "55px", width: "55px", margin: "2px", fontSize: "30px"};
   else if(n===10) style = {height: "45px", width: "45px", margin: "1px", fontSize: "25px"};
   else if(n===12) style = {height: "37px", width: "37px", margin: "1px", fontSize: "23px"};
+
   // Only changes value if tile is mutable
   function handleClick(){
-    if(mutability)
+    if(mutability){
       setValue((value+1)%3);
+      let row = pos/n;
+      let col = pos%n;
+      setVal(value, row, col);
+    }
   }
 
   function getDisplay(){
@@ -28,7 +35,7 @@ function Tile({value_, mutability, n}){
     switch(value){
       case 0: color = (mutability)?'Zero':'ZeroImmutable'; break;
       case 1: color = (mutability)?'One':'OneImmutable'; break;
-      default: color = 'Null'
+      default: color = 'Null';
     }
   }
   changeColor(); // Sets color when component is created
