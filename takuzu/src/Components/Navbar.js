e5172a8bb035dfd60be37f1919f306bd55d17d88
daPots <srivaths.ravva@gmail.com>
2023-12-28 01:01:38 -0500

    Theme Toggle
    Toggle works pretty much only on navbar. Need to figure out how to change everything, especially page background color.

--- takuzu/src/Components/Navbar.js
+++ takuzu/src/Components/Navbar.js
@@ -1,6 +1,6 @@
 // React imports
 import {NavLink} from 'react-router-dom'
-import { useState } from 'react'
+import { useState, createContext } from 'react'
 import music from '../music.wav'
 
 // Bootstrap imports
@@ -8,6 +8,8 @@ import Nav from 'react-bootstrap/Nav'
 import NavBar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Toggle from './Toggle'
+
+export const ThemeContext = createContext(null);
 
 function NavigationBar(){
   const [audio, setAudio] = useState(new Audio(music));
@@ -23,29 +25,36 @@ function NavigationBar(){
     }
   }
 
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return(
-    <NavBar>
-      <div className='ms-4'>
-      <NavLink to='/' className='nav-link'>
-        <NavBar.Brand>
-          <img src={process.env.PUBLIC_URL + '/namelogo.png'} width="100" height="30" alt="Logo"></img>
-        </NavBar.Brand>
-      </NavLink>
-      </div>
-      <Nav>
-        <NavDropdown title='Puzzles' style={{color: 'black'}}>
-          <NavLink to='/6x6puzzle' className='dropdown-item'>6x6 Puzzles</NavLink>
-          <NavLink to='/8x8puzzle' className='dropdown-item'>8x8 Puzzles</NavLink>
-          <NavLink to='/10x10puzzle' className='dropdown-item'>10x10 Puzzles</NavLink>
-          <NavLink to='/12x12puzzle' className='dropdown-item'>12x12 Puzzles</NavLink>
-        </NavDropdown>
-        <NavLink to='/tutorial' className='nav-link' style={{color: 'black'}}> Tutorial </NavLink>
-        <NavLink to='/settings' className='nav-link' style={{color: 'black'}}> Settings </NavLink>
-        <NavLink to='/about' className='nav-link' style={{color: 'black'}}> About </NavLink>
-        <Toggle label=" "/>
-        <button id='sound' onClick={playPause}>{playing ? '\u23f8' : '\u266B'}</button>
-      </Nav>
-    </NavBar>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <NavBar id={theme}>
+        <div className='ms-4'>
+        <NavLink to='/' className='nav-link'>
+          <NavBar.Brand>
+            <img src={process.env.PUBLIC_URL + '/namelogo.png'} width="100" height="30" alt="Logo"></img>
+          </NavBar.Brand>
+        </NavLink>
+        </div>
+        <Nav>
+          <NavDropdown title='Puzzles' style={{color: 'black'}}>
+            <NavLink to='/6x6puzzle' className='dropdown-item'>6x6 Puzzles</NavLink>
+            <NavLink to='/8x8puzzle' className='dropdown-item'>8x8 Puzzles</NavLink>
+            <NavLink to='/10x10puzzle' className='dropdown-item'>10x10 Puzzles</NavLink>
+            <NavLink to='/12x12puzzle' className='dropdown-item'>12x12 Puzzles</NavLink>
+          </NavDropdown>
+          <NavLink to='/tutorial' className='nav-link'> Tutorial </NavLink>
+          <NavLink to='/settings' className='nav-link'> Settings </NavLink>
+          <NavLink to='/about' className='nav-link'> About </NavLink>
+          <Toggle label=" " onToggle={toggleTheme}/>
+          <button id='sound' onClick={playPause}>{playing ? '\u23f8' : '\u266B'}</button>
+        </Nav>
+      </NavBar>
+    </ThemeContext.Provider>
   );
 }
 //263C for sun, 263D for moon, 2699 for gear
--- takuzu/src/Components/Toggle.js
+++ takuzu/src/Components/Toggle.js
@@ -1,11 +1,18 @@
 import "./Toggle.css"
 
-const Toggle = ({ label }) => {
+const Toggle = ({ label, onToggle }) => {
+    const handleToggle = () => {
+        // Call the onToggle function if it exists
+        if (onToggle) {
+          onToggle();
+        }
+      };
+
     return (
         <div className="container">
             {label}{""}
             <div className="toggle-switch">
-                <input type="checkbox" className="checkbox" name={label} id={label} />
+                <input type="checkbox" className="checkbox" name={label} id={label} onChange={handleToggle}/>
                 <label className="label" htmlFor={label}>
                     <span className="inner" />
                     <span className="switch" />
